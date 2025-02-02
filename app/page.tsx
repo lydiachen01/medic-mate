b38854c2d3a02b23bfb1d2908ac38f8bbba97bdb
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2025-02-02 11:38:22 -0500

    Fixed deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-'use client'
-
 import Welcome from "@/components/Homepage/Welcome";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import i18n from "@/utils/i18n"; // Assuming you have an i18n setup
+// import i18n from "@/utils/i18n"; // Assuming you have an i18n setup
 
 export default async function Home() {
-  const [language, setLanguage] = useState("en");
-  const router = useRouter();
+  // const [language, setLanguage] = useState("en");
+  // const router = useRouter();
 
-  useEffect(() => {
-    const userLang = navigator.language || navigator.userLanguage;
-    if (userLang.startsWith("es")) {
-      setLanguage("es");
-      i18n.changeLanguage("es");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const userLang = navigator.language || navigator.userLanguage;
+  //   if (userLang.startsWith("es")) {
+  //     setLanguage("es");
+  //     // i18n.changeLanguage("es");
+  //   }
+  // }, []);
 
   const supabase = await createClient();
   const { data: { session } } = await supabase.auth.getSession();
@@ -26,5 +24,5 @@ export default async function Home() {
     return redirect("/home");
   } 
 
-  return <Welcome language={language} />;
+  return <Welcome />;
 };
