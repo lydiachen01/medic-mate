f725a7b273471a88f2715f066fcec363d60c767e
helena-f <43306951+helena-f@users.noreply.github.com>
2025-02-02 10:15:39 -0500

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily:{
+        inter: ['Itim', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
